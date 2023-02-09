fdfa9e0adf62726086a62ee7cdf08a41c661e8e7
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-02-09 13:14:05 +0000

    add SeedData

---
+++ Translator/Models/SeedTranslationsData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Translator.Data;
+using System;
+using System.Linq;
+
+namespace Translator.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new TranslatorContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<TranslatorContext>>()))
+        {
+            // Look for any movies.
+            if (context.Translation.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Translation.Add(
+                new Translation
+                {
+                    Name = "Leetspeak",
+                    Url = "https://api.funtranslations.com/translate/leetspeak.json",
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
--- Translator/Program.cs
+++ Translator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Translator.Data;
+using Translator.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TranslatorContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TranslatorContext") ?? throw new InvalidOperationException("Connection string 'TranslatorContext' not found.")));
@@ -9,6 +11,13 @@ builder.Services.AddDbContext<TranslatorContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
