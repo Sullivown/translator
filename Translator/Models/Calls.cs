daee1b1c7c154fbc1a5f500821c7fb1d99529817
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-02-10 12:18:52 +0000

    add Calls class

---
+++ Translator/Models/Calls.cs
@@ -0,0 +1,13 @@
+﻿namespace Translator.Models
+{
+    public class Calls
+    {
+        public int Id { get; set; }
+        public string? OriginalText { get; set; }
+        public string? TranslatorType { get; set; }
+        public bool IsSuccessful { get; set; }
+        public string? TranslatedText { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
